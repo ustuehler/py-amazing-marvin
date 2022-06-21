cd60aa12b2d1157568baeeecdcf55a53ba685f70
Uwe Stuehler <uwe@bsdx.de>
2022-06-22 07:09:33 +0900

    feat: implement /me endpoint

--- src/amazing_marvin/__init__.py
+++ src/amazing_marvin/__init__.py
@@ -1,8 +1,21 @@
+import json
+
+import requests as requests
+
+API_URL = 'https://serv.amazingmarvin.com/api'
+
+
 class AmazingMarvinAPI:
     def __init__(self, api_token: str):
-        self._api_token = api_token
+        self._session = requests.Session()
+        self._session.headers['X-API-Token'] = api_token
 
     def me(self) -> dict[str, any]:
-        return {
-            'email': "user@example.com"
-        }
+        return self._get_json_response_data('/me')
+
+    def _get_json_response_data(self, path: str) -> any:
+        response = self._session.get(f"{API_URL}{path}")
+        response.raise_for_status()
+
+        data = json.loads(response.content)
+        return data
